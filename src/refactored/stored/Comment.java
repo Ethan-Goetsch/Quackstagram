c4cd2559d5adad39d7e56cd103ded08f70f1541c
EthanGoetsch <ethangoetschhome@gmail.com>
2024-03-03 21:24:23 +0100

    Fixed package usages

---
+++ src/refactored/stored/Comment.java
@@ -0,0 +1,9 @@
+package refactored.stored;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record Comment(User user, Content content, LocalDateTime DateCreated, List<Comment> Comments)
+{
+
+}
