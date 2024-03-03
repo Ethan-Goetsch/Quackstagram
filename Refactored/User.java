f3e368687d6ea127b46ce85067a5d96289a483f4
EthanGoetsch <ethangoetschhome@gmail.com>
2024-03-03 13:46:47 +0100

    Modified Picture and User to work with Posts, Comments and Content

--- Refactored/User.java
+++ Refactored/User.java
@@ -1,61 +1,42 @@
 package Refactored;
 
-import java.util.ArrayList;
 import java.util.List;
 
 // Represents a user on Quackstagram
 class User
 {
-    private String username;
-    private String bio;
-    private String password;
-    private int postsCount;
-    private int followersCount;
-    private int followingCount;
-    private List<Picture> pictures;
+    private final int id;
 
-    public User(String username, String bio, String password)
+    private final List<Post> posts;
+    private final List<User> followers;
+    private final List<User> followings;
+
+    private String username;
+    private String password;
+    private String bio;
+
+    public User(int id, String username, String password, String bio, List<Post> posts, List<User> followers, List<User> followings)
     {
+        this.id = id;
         this.username = username;
         this.bio = bio;
         this.password = password;
-        this.pictures = new ArrayList<>();
-        // Initialize counts to 0
-        this.postsCount = 0;
-        this.followersCount = 0;
-        this.followingCount = 0;
+        this.posts = posts;
+        this.followers = followers;
+        this.followings = followings;
     }
 
-    public User(String username)
-    {
-        this.username = username;
-    }
-
-    // Add a picture to the user's profile
-    public void addPicture(Picture picture)
-    {
-        pictures.add(picture);
-        postsCount++;
-    }   
-
-    // Getter methods for user details
+    public int getID() { return id; }
     public String getUsername() { return username; }
+    public String getPassword() { return password; }
     public String getBio() { return bio; }
-    public int getPostsCount() { return postsCount; }
-    public int getFollowersCount() { return followersCount; }
-    public int getFollowingCount() { return followingCount; }
-    public List<Picture> getPictures() { return pictures; }
-    
-    // Setter methods for followers and following counts
-    public void setBio(String bio) {this.bio = bio; }
-    public void setFollowersCount(int followersCount) { this.followersCount = followersCount; }
-    public void setFollowingCount(int followingCount) { this.followingCount = followingCount; }
-    public void setPostCount(int postCount) { this.postsCount = postCount;}
-    
-    // Implement the toString method for saving user information
+    public List<Post> getPosts() { return posts; }
+    public List<User> getFollowers() { return followers; }
+    public List<User> getFollowings() { return followings; }
+
     @Override
     public String toString()
     {
-        return username + ":" + bio + ":" + password; // Format as needed
+        return "[User]: " + "Username: " + username;
     }
 }
