72fd0fbe136a810b76c574ab4001d19c2908fe3c
EthanGoetsch <ethangoetschhome@gmail.com>
2024-03-03 14:37:01 +0100

    Added LocalDateTime to Content

--- Refactored/Content.java
+++ Refactored/Content.java
@@ -1,21 +1,28 @@
 package Refactored;
 
+import java.time.LocalDateTime;
+
 public class Content
 {
     private String text;
     private Picture picture;
+    private LocalDateTime dateCreated;
 
-    private Content(String text, Picture picture)
+    private Content(String text, Picture picture, LocalDateTime dateCreated)
     {
+        this.dateCreated = dateCreated;
         this.text = text;
         this.picture = picture;
     }
 
     public String getText() { return text; }
     public Picture getPicture() { return picture; }
+    public LocalDateTime getDateCreated() { return dateCreated; }
 
     public void setText(String text) { this.text = text; }
     public void setPicture(Picture picture) { this.picture = picture; }
+
+    private void setDateCreated(LocalDateTime dateCreated) {this.dateCreated = dateCreated; }
 
     public class Builder
     {
@@ -24,7 +31,7 @@ public class Content
 
         public Builder()
         {
-            content = new Content("", null);
+            content = new Content("", null, LocalDateTime.now());
             context = this;
         }
 
@@ -37,6 +44,12 @@ public class Content
         public Builder withPicture(Picture picture)
         {
             content.setPicture(picture);
+            return context;
+        }
+
+        public Builder withDate(LocalDateTime dateTime)
+        {
+            content.setDateCreated(dateTime);
             return context;
         }
 
